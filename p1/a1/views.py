1795ebd49112b2539ce3f7eee7c4114d8ecececf
vishal <vishal@gmail.com>
2023-07-09 23:13:55 +0530

    views commited

--- p1/a1/views.py
+++ p1/a1/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
     return HttpResponse("hello World")
+
+def index2(request):
+    t=loader.get_template("index.html")
+    return HttpResponse(t.render())
